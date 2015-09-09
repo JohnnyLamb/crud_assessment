d0739db579b458d3f2df66c7dd5c8ddf857e6130
johnny lamb <agnusjack@gmail.com>
2015-09-09 15:18:01 -0600

    added mongo uri

--- server/models/animals.js
+++ server/models/animals.js
@@ -9,6 +9,6 @@ var Animal = new Schema(
 
 process.env.DB_HOST = 'mongodb://localhost/animals';
 
-mongoose.connect(process.env.DB_HOST);
+mongoose.connect(process.env.DB_HOST || 'mongodb://heroku_9vt4f50f:939ukdae6tqj2c1d1gqjj834mq@ds053198.mongolab.com:53198/heroku_9vt4f50f');
 
 module.exports = mongoose.model('animals', Animal);
